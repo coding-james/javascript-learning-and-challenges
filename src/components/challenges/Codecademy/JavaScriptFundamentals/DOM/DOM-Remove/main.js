b78fe87ac879a76838c5e81f662cfca85594e760
James <98489957+coding-james@users.noreply.github.com>
2023-11-03 18:04:02 +0000

    DOM Remove an element

---
+++ src/components/challenges/Codecademy/JavaScriptFundamentals/DOM/DOM-Remove/main.js
@@ -0,0 +1,5 @@
+// Select element with ID of vespa
+let elementToRemove = document.getElementById('vespa');
+
+// Remove the elementToRemove element from its parent - id of italy-attractions
+document.getElementById("italy-attractions").removeChild(elementToRemove);
