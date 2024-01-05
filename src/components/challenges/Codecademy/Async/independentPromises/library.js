05b033ff88c81a743582717281ed228b73d8d5e5
James <98489957+coding-james@users.noreply.github.com>
2024-01-05 21:59:09 +0000

    async Independent Promises

---
+++ src/components/challenges/Codecademy/Async/independentPromises/library.js
@@ -0,0 +1,31 @@
+export let cookBeans = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('beans');
+        }, 1000);
+    });
+}
+
+export let steamBroccoli = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('broccoli');
+        }, 1000);
+    });
+}
+
+export let cookRice = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('rice');
+        }, 1000);
+    });
+}
+
+export let bakeChicken = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('chicken');
+        }, 1000);
+    });
+}
